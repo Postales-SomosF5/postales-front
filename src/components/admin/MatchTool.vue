df818c2713f4a3cc8f0b8172c39041424bbd11d2
silvia76-max <silviarodriguezexposito.76@gmail.com>
2025-05-23 09:30:43 +0200

    borrado archivos antiguos

--- src/components/admin/MatchTool.vue
+++
@@ -1,8 +0,0 @@
-import { generarMatches } from '@/services/matchAlgorithm.js'
-
-const props = defineProps({ usuarios: Array })
-const suggestedMatches = ref([])
-
-onMounted(() => {
-  suggestedMatches.value = generarMatches(props.usuarios)
-})
